d14020d1df4f27c6b60655e775b84b359fa0e509
Antidote1911 <antidote1911@gmail.com>
2022-04-02 19:24:27 +0200

    initial

---
+++ src/cli.rs
@@ -0,0 +1,182 @@
+use crate::config::Config;
+
+use clap::{AppSettings, Parser};
+use passgen_core::{calculate_entropy, Pool};
+use std::io::Write;
+
+#[derive(Parser, Debug)]
+#[clap(author, about, version,
+after_help = "If you do not specify any of the \
+[--uppercase, --lowercase, --digits, --symbols, --others] flags, \
+then uppercase, lowercase letters and digits will be used.",
+setting = AppSettings::DeriveDisplayOrder)]
+pub struct Cli {
+    /// Use UPPERCASE letters [A-Z]
+    #[clap(short, long)]
+    uppercase: bool,
+
+    /// Use lowercase letters [a-z]
+    #[clap(short, long)]
+    lowercase: bool,
+
+    /// Use digits [0-9]
+    #[clap(short, long)]
+    digits: bool,
+
+    /// Use special symbols [*&^%$#@!~]
+    #[clap(short, long)]
+    symbols: bool,
+
+    /// Use other symbols [♕♖♗♘♙♚...].
+    #[clap(short, long)]
+    others: bool,
+
+    /// Sets the required password length
+    #[clap(short = 'L', long, value_name = "NUMBER", default_value = "12")]
+    length: usize,
+
+    /// Sets the minimum required password entropy (conflicts with --length)
+    #[clap(short = 'E', long, value_name = "NUMBER", conflicts_with = "length")]
+    entropy: Option<f64>,
+
+    /// Number of passwords
+    #[clap(short, long, value_name = "NUMBER", default_value = "1")]
+    count: usize,
+
+    /// Prints password information
+    #[clap(short, long)]
+    info: bool,
+
+    /// Output in a txt file
+    #[clap(long)]
+    output: Option<String>,
+
+    /// Sets config to default values
+    #[clap(long = "config")]
+    reset: bool,
+
+    #[clap(skip = Config::new())]
+    config: Config,
+}
+
+impl Cli {
+    pub fn collect(&self) -> Pool {
+        let mut pool = Pool::new();
+
+        if self.uppercase {
+            pool.extend_from_string(self.config.uppercase());
+        }
+        if self.lowercase {
+            pool.extend_from_string(self.config.lowercase());
+        }
+        if self.digits {
+            pool.extend_from_string(self.config.digits());
+        }
+        if self.symbols {
+            pool.extend_from_string(self.config.symbols());
+        }
+        if self.others {
+            pool.extend_from_string(self.config.others());
+        }
+
+        if pool.is_empty() {
+            pool.extend_from_string(self.config.default_set());
+        }
+
+        pool
+    }
+
+    pub fn count(&self) -> usize {
+        self.count
+    }
+
+    pub fn reset(&self) -> bool {
+        self.reset
+    }
+
+    pub fn entropy(&self) -> Option<f64> {
+        self.entropy
+    }
+
+    pub fn length(&self) -> usize {
+        self.length
+    }
+
+    pub fn info(&self) -> bool {
+        self.info
+    }
+
+    pub fn output(&self) -> Option<String> {
+        self.output.clone()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Info {
+    entropy: f64,
+    length: usize,
+    pool_size: usize,
+}
+
+impl Info {
+    // Creates new instance
+    pub fn new(length: usize, pool_size: usize) -> Self {
+        let entropy = calculate_entropy(length, pool_size);
+
+        Info {
+            entropy,
+            length,
+            pool_size,
+        }
+    }
+
+    // Prints info
+    pub fn write(&self, mut writer: impl Write) {
+        writeln!(
+            writer,
+            "Entropy: {:.0} bits | Length: {} chars | Pool size: {} chars",
+            self.entropy, self.length, self.pool_size
+        )
+        .unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn opts_collect() {
+        let opts = Cli {
+            uppercase: true,
+            lowercase: true,
+            digits: true,
+            symbols: true,
+            others: true,
+            length: 0,
+            entropy: None,
+            count: 1,
+            info: false,
+            output: None,
+            reset: false,
+            config: Config::default(),
+        };
+        let pool = opts.collect();
+
+        assert!(pool.contains_all(opts.config.uppercase()));
+        assert!(pool.contains_all(opts.config.lowercase()));
+        assert!(pool.contains_all(opts.config.digits()));
+        assert!(pool.contains_all(opts.config.symbols()));
+        assert!(pool.contains_all(opts.config.others()));
+    }
+
+    #[test]
+    fn info_write() {
+        let mut actual: Vec<u8> = vec![];
+        Info::new(15, 64).write(&mut actual);
+
+        let expected = b"Entropy: 90 bits | Length: 15 chars | Pool size: 64 chars\n".to_vec();
+
+        assert_eq!(actual, expected);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use crate::cli::{Cli, Info};
+use crate::config::Config;
+use clap::Parser;
+use passgen_core::{calculate_length, generate_n_passwords};
+use std::io::stdout;
+use std::{fs::File, io::LineWriter, io::Write};
+
+use color_eyre::eyre::{Result, eyre};
+use color_eyre::owo_colors::OwoColorize;
+
+extern crate question;
+
+use question::{Answer, Question};
+
+pub mod cli;
+pub mod config;
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{} {}", "Error :".red(), e.red());
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let opts: Cli = Cli::parse();
+
+    if opts.reset() {
+        Config::save_default().map_err(|e| eyre!(e))?;
+        println!("{}", "The default configuration was set successfully !".green());
+        std::process::exit(0);
+    }
+
+    let pool = opts.collect();
+
+    let length = opts.entropy().map_or(opts.length(), |e| {
+        calculate_length(e, pool.len() as f64) as usize
+    });
+
+    let pass_vec = generate_n_passwords(&pool, length, opts.count());
+
+    for n in 0..opts.count() {
+        println!("{}", pass_vec[n].yellow());
+    }
+
+    if opts.output().is_some() {
+        let dest = opts.output().unwrap();
+        if std::path::Path::new(&dest).is_dir() && &dest != "/dev/null" {
+            return Err(eyre!("Can't save file. A folder with this name exist."));
+        }
+        if std::path::Path::new(&dest).exists() && &dest != "/dev/null" {
+            println!("File: '{}' exist.", &dest);
+            let answer = Question::new("Try to Overwrite ?")
+                .default(Answer::NO)
+                .show_defaults()
+                .confirm();
+
+            if answer == Answer::YES {
+                writetxt(pass_vec, &dest).map_err(|e| eyre!(e))?;
+                println!("{} '{}' {}", "File".green(), opts.output().unwrap().green(), "was overwritten.".green());
+            } else {
+                println!("{}", "Writting file canceled.".green());
+            }
+        } else {
+            writetxt(pass_vec, &dest)?;
+        }
+    }
+
+    if opts.info() {
+        Info::new(length, pool.len()).write(stdout());
+    }
+    Ok(())
+}
+
+fn writetxt(x: Vec<String>, dest: &String) -> Result<()> {
+    let file = File::create(dest)?;
+    let mut file = LineWriter::new(file);
+
+    for i in x {
+        file.write_all(i.as_bytes())?;
+        file.write_all(b"\n")?;
+    }
+    Ok(())
+}
